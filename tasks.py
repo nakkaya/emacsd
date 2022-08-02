07b6d32bc396e29fa7c225b29b85f97d44051e0e
Nurullah Akkaya <nurullah@nakkaya.com>
2022-08-02 23:46:40 +0300

    Update:   .github/workflows/main.yml   tasks.py

--- tasks.py
+++ tasks.py
@@ -2,12 +2,6 @@
 
 from invoke import task
 import subprocess
-import os
-import sys
-sys.tracebacklimit = 0
-
-version = subprocess.check_output(["git", "describe", "--always"])
-version = version.strip().decode('UTF-8')
 
 
 def tag(n):
@@ -15,42 +9,30 @@ def tag(n):
     return ("--tag ghcr.io/nakkaya/" + n + ":latest ")
 
 
-def run(cmd, dir="."):
-    """Run cmd in dir."""
-    wd = os.getcwd()
-    os.chdir(dir)
-    subprocess.check_call(cmd, shell=True)
-    os.chdir(wd)
-
-
 gpu_image = 'BASE_IMAGE=nvidia/cuda:11.2.0-cudnn8-runtime-ubuntu20.04'
 
 
-@task
-def build(ctx):
-    """Build Images."""
-    cmd = "docker build "
-    # cmd = "docker build --no-cache "
-
-    run(cmd + "-f Dockerfile " + tag("emacsd-cpu") + " .")
-
-
-buildx_cmd = "docker buildx build --push "
+def docker(builder, type, *arg):
+    """Run docker command."""
+    cmd = ("docker " + builder +
+           " -f Dockerfile " + tag("emacsd-" + type) +
+           " ".join(arg) + " .")
+    subprocess.check_call(cmd, shell=True)
 
 
 @task
-def buildx_gpu(ctx):
-    """Build adm64 Image."""
+def build_cpu(ctx):
+    """Build CPU Image."""
+    docker("build", "cpu")
 
-    run(buildx_cmd +
-        " -f Dockerfile " + tag("emacsd-gpu") +
-        " --platform linux/amd64 " +
-        " --build-arg " + gpu_image + " .")
+
+@task
+def build_gpu(ctx):
+    """Build GPU Image."""
+    docker("build", "gpu", "--build-arg", gpu_image)
 
 
 @task
 def buildx_cpu(ctx):
-    """Build amd64 CPU Image."""
-    run(buildx_cmd +
-        "-f Dockerfile " + tag("emacsd-cpu") +
-        " --platform linux/amd64,linux/arm64 .")
+    """Build Multi Arch CPU Image."""
+    docker("buildx build --push", "cpu", "--platform linux/amd64,linux/arm64")
